ad59f631f218a20e93710459b4d217868fb3c9e2
Asim Can Yagiz <meycasim@gmail.com>
2022-09-24 00:03:46 +0300

    Struct for save informations about User

--- Sword-Sandals-Bazaar/Sword-Sandals-Bazaar/Model/User.swift
+++ Sword-Sandals-Bazaar/Sword-Sandals-Bazaar/Model/User.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class User {
+    var name: String
+    var age: Int
+    var gender: String
+    var job: String
+    var gold = 100
+    
+    init(name: String, age: Int, gender: String, job: String, gold: Int = 100) {
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.job = job
+        self.gold = gold
+    }
+    
+}
