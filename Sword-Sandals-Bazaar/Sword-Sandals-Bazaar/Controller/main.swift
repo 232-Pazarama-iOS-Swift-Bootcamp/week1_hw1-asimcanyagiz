40e32556125feb44c993562d40987a189bb8cd27
Asim Can Yagiz <meycasim@gmail.com>
2022-09-24 00:04:44 +0300

    Main interface builded for run' all events in terminal

--- Sword-Sandals-Bazaar/Sword-Sandals-Bazaar/Controller/main.swift
+++ Sword-Sandals-Bazaar/Sword-Sandals-Bazaar/Controller/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+var fun = Functions()
+
+func start() {
+    print("Dou you want a play game 'yes' or 'no'")
+    let playGame = readLine()
+    
+    
+    //MARK: User informations
+    var name: String
+    var age: Int
+    var gender: String
+    var job: String
+    
+    
+    if playGame?.lowercased() == "yes" {
+        //MARK: Loop for take information from user
+        name = fun.getUserName()
+        age = fun.getUserAge()
+        gender = fun.getUserGender()
+        job = fun.getUserJob()
+        var user = User(name: name, age: age, gender: gender, job: job)
+        print("Name: \(name) Age: \(age) Gender: \(gender) Job: \(job)")
+        
+        
+        
+    } else {
+        print("Welcome to our secret bazaar. If you want join the this adventure you need the write 'yes' ")
+        start()
+    }
+}
+start()
+
 
